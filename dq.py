d24392768e47e310408cea06ce1323228e7c56fa
JosephPascual <123832660+JosephPascual@users.noreply.github.com>
2025-10-15 21:18:44 -0700

    Create dq.py
    genesis

---
+++ dq.py
@@ -0,0 +1,10 @@
+st.sidebar.title("Navigation")
+
+page = st.sidebar.selectbox('Choose a section', ['About Us', 'FAQs', 'Submit Feedback'])
+
+if page == 'About Us':
+    st.header("About Us")
+if page == 'FAQs':
+    st.header("FAQs")
+if page == 'Submit Feedback':
+    st.header("Submit Feedback")
