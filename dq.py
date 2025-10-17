2d86d1058e2314925a08769ca66530251d42a6dd
JosephPascual <123832660+JosephPascual@users.noreply.github.com>
2025-10-16 21:37:49 -0700

    Added expanders to FAQ

--- dq.py
+++ dq.py
@@ -24,6 +24,13 @@ if page == 'About Us':
 
 if page == 'FAQs':
         st.header("FAQs")
+        bean_source_expander = st.expander("Where do you source your coffee beans?")
+        with bean_source_expander:
+            st.write('Our coffee beans are ethically sourced from family-owned farms and cooperatives across various coffee-growing regions, ensuring quality and sustainability in every cup.')
+        roast_expander = st.expander('How do you roast your beans?')
+        with roast_expander:
+            st.write("We employ a combination of traditional and modern roasting techniques, meticulously adjusting the roast profile for each batch to bring out the unique flavors and aromas of the beans.")
+
 if page == 'Submit Feedback':
     st.header("Submit Feedback")
 
