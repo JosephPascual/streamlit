749bbb833089a87861ef261e580cc31349e42c35
JosephPascual <123832660+JosephPascual@users.noreply.github.com>
2025-10-16 20:56:28 -0700

    Update dq.py
    removed 'st' from bugged lines

--- dq.py
+++ dq.py
@@ -9,11 +9,11 @@ if page == 'About Us':
     # Add your columns here
     col1, col2, col3 = st.columns(3)
     col1.image("https://s3.amazonaws.com/dq-content/901/Charles_Bingley.png")
-    col1.st.header("Charles Bingley")
+    col1.header("Charles Bingley")
     col2.image("https://s3.amazonaws.com/dq-content/901/Elizabeth_Bennet.png")
-    col2.st.header('Elizabeth Bennet')
+    col2.header('Elizabeth Bennet')
     col3.image("https://s3.amazonaws.com/dq-content/901/Georgiana_Darcy.png")
-    col3.st.header('Georgiana_Darcy.png')
+    col3.header('Georgiana_Darcy.png')
 if page == 'FAQs':
     st.header("FAQs")
 if page == 'Submit Feedback':
