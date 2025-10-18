d2ce20593733389197bcd76fbc398c210a1b3350
JosephPascual <123832660+JosephPascual@users.noreply.github.com>
2025-10-18 11:59:40 -0700

    updated fact function

--- dq.py
+++ dq.py
@@ -30,8 +30,22 @@ def general_faqs():
     roast_expander = st.expander('How do you roast your beans?')
     with roast_expander:
         st.write("We employ a combination of traditional and modern roasting techniques, meticulously adjusting the roast profile for each batch to bring out the unique flavors and aromas of the beans.")
+
+def recipe_faqs():
+    recommended = st.expander('What is your recommended recipe for a classic cold brew coffee?', )
+    with recommended:
+        st.write('For a smooth and robust cold brew, mix coarsely ground coffee beans with cold water in a 1:8 ratio, steep for 12-18 hours, and then filter. Serve over ice and customize with milk or sweeteners to taste.')
+    signature = st.expander('Do you have a signature coffee-based dessert recipe?')
+    with signature:
+        st.write('Yes, our signature dessert is the \'GlobalJava Mocha Brownies.\' Blend melted dark chocolate with your favorite GlobalJava espresso shot, add to your brownie mix, and bake. These rich, coffee-infused brownies are a coffee lover\'s delight and perfect for any occasion.')
+
 if page == 'FAQs':
-    general_faqs()
+    st.header('FAQs')
+    tab1, tab2 = st.tabs(['General', 'Recipes'])
+    with tab1:
+        general_faqs()    
+    with tab2:
+        recipe_faqs()
 
 if page == 'Submit Feedback':
     st.header('Submit Feedback')
