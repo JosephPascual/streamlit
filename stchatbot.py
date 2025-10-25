7ec7a988eb08435a592cf51b44733f9fcf725d77
JosephPascual <123832660+JosephPascual@users.noreply.github.com>
2025-10-25 11:47:45 -0700

    added return statements

--- stchatbot.py
+++ stchatbot.py
@@ -15,20 +15,23 @@ DEFAULT_TOKEN_BUDGET = 4096
 # Classes
 class ConverstaionManager:
     def __init__(self):
-        return
+        return None
     
     def set_persona(self):
         # Adjust the chatbot's personality
-        
+        return None
+
     def set_custom_system_message(self):
         # Allows users to set a customer system message
-        
+        return None
+    
     def chat_completion(self):
         # Generates responses based on user input
-        
+        return None
+
     def reset_conversation_history(self):
         # Clears chat history
-        
+        return None
         
         
 # Working Code Below
